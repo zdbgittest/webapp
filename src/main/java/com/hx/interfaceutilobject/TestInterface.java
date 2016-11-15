4ca110ac0ae5a1e9702a3518d3ea586bceba1bcd
zyq <zyq@qq.com>
2016-11-15 09:41:28 +0800

    自定义注解interfaceutilobject

---
+++ src/main/java/com/hx/interfaceutilobject/TestInterface.java
@@ -0,0 +1,21 @@
+package com.hx.interfaceutilobject;
+
+/** 
+ * 一个注解的测试类 
+ * @author zhuli 
+ * @date 2014-7-5 
+ */  
+//注入注解作用于类上面  
+//可以通过反射 获取类的信息之后 获取得到这个注解的值  
+@UserNameAnnotations(value = "initphp") 
+public class TestInterface {
+	private String userName;  
+	  
+    public String getUserName() {  
+        return userName;  
+    }  
+  
+    public void setUserName(String userName) {  
+        this.userName = userName;  
+    }  
+}
