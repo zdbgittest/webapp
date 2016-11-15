fb7ea6896665b65061a2440204c9ccf919bbd703
zyq <zyq@qq.com>
2016-11-15 09:52:48 +0800

    提交的是作用在方法上的注解全在interfaceutilmethod包中

---
+++ src/main/java/com/hx/interfaceutilmethod/MainTest.java
@@ -0,0 +1,35 @@
+package com.hx.interfaceutilmethod;
+
+import java.lang.reflect.Method;
+
+import com.hx.interfaceutilmethod.MethodType.MethodTypeEnum;
+
+public class MainTest {
+	public static void main(String[] args) {  
+        Class<Test> testClass = Test.class;  
+        try {  
+            //因为是注解到method上的，所以首先要获取这个方法  
+            Method method = testClass.getDeclaredMethod("getUserName");  
+              
+            //判断这个方法上是否有这个注解  
+            if (method.isAnnotationPresent(MethodType.class)) {  
+                System.out.println("this is a method Annotation");  
+                  
+                //如果有这个注解，则获取注解类  
+                MethodType methodType = (MethodType) method.getAnnotation(MethodType.class);  
+                if (methodType != null) {  
+                    if (MethodTypeEnum.TYPE1.equals(methodType.methodType())) {  
+                        System.out.println("this is TYPE1");  
+                    } else {  
+                        System.out.println("this is TYPE2");  
+                    }  
+                }  
+            } else {  
+                System.out.println("this is not  a method Annotation");  
+            }  
+  
+        } catch (Exception e) {  
+        }  
+  
+    }
+}
---
+++ src/main/java/com/hx/interfaceutilmethod/Test.java
@@ -0,0 +1,26 @@
+package com.hx.interfaceutilmethod;
+
+import com.hx.interfaceutilmethod.MethodType.MethodTypeEnum;
+
+/** 
+ * 一个注解的测试类 
+ * @author zhuli 
+ * @date 2014-7-5 
+ */  
+//注入注解作用于类上面  
+//可以通过反射 获取类的信息之后 获取得到这个注解的值  
+public class Test {  
+  
+    private String userName;  
+  
+    //注解到  
+    @MethodType(methodType=MethodTypeEnum.TYPE2)  
+    public String getUserName() {  
+        return userName;  
+    }  
+  
+    public void setUserName(String userName) {  
+        this.userName = userName;  
+    }  
+  
+} 
